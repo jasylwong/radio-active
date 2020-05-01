0039999acc06f89bb1234633ce6640ddf0a3eecf
Jason <jasylwong@gmail.com>
2020-05-01 11:45:54 +0100

    add test for two sounds outside range

--- JavaScript/band_pass_filter.js
+++ JavaScript/band_pass_filter.js
@@ -1,9 +1,17 @@
 const band_pass_filter = (soundsArr, min, max) => {
-  if (soundsArr[0] >= min && soundsArr[0] <= max) {
-    return soundsArr;
-  } else if (soundsArr[0] < min) {
-    return [min];
-  }
+  
+  let output = [];
+
+  soundsArr.forEach((sound) => {
+    if (sound >= min && sound <= max) {
+      output.push(sound);
+    } else if (sound < min) {
+      output.push(min);
+    } else {
+      output.push(max);
+    }
+  })
+  return output;
 }
 
 module.exports = band_pass_filter
--- JavaScript/band_pass_filter.test.js
+++ JavaScript/band_pass_filter.test.js
@@ -17,5 +17,29 @@ describe('band_pass_filter', () => {
     test('single sound below range gets set at min', () => {
       expect(band_pass_filter([25], 40, 45)).toEqual([40]);
     })
+
+    test('single sound above range gets set at max', () => {
+      expect(band_pass_filter([200], 40, 45)).toEqual([45]);
+    })
+  })
+
+  describe('two sounds', () => {
+    describe('two sounds within range' , () => {
+      test('two sounds within range', () => {
+        expect(band_pass_filter([41, 42], 40, 45)).toEqual([41, 42]);
+      })
+    })
+
+    describe('two sounds: one within range, one above' , () => {
+      test('two sounds within range', () => {
+        expect(band_pass_filter([41, 60], 40, 45)).toEqual([41, 45]);
+      })
+    })
+
+    describe('two sounds: both outside range' , () => {
+      test('two sounds within range', () => {
+        expect(band_pass_filter([5, 46], 40, 45)).toEqual([40, 45]);
+      })
+    })
   })
 })

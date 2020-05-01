6396ad1c429356388ad85ae25019822e777542fa
Jason <jasylwong@gmail.com>
2020-05-01 11:23:18 +0100

    add below min test

--- JavaScript/band_pass_filter.js
+++ JavaScript/band_pass_filter.js
@@ -1,10 +1,9 @@
 const band_pass_filter = (soundsArr, min, max) => {
-  if (soundsArr === [45]) {
-    return [45];
-  } else {
-    return [5];
+  if (soundsArr[0] >= min && soundsArr[0] <= max) {
+    return soundsArr;
+  } else if (soundsArr[0] < min) {
+    return [min];
   }
-
 }
 
 module.exports = band_pass_filter
--- JavaScript/band_pass_filter.test.js
+++ JavaScript/band_pass_filter.test.js
@@ -2,20 +2,20 @@ const band_pass_filter = require("./band_pass_filter")
 
 describe('band_pass_filter', () => {
   describe('single sound', () => {
-    test('single sound within range gets returned', () => {
+    test('45 within range gets returned', () => {
       expect(band_pass_filter([45], 30, 50)).toEqual([45]);
     })
 
-    test('single sound within range gets returned', () => {
+    test('5 within range gets returned', () => {
       expect(band_pass_filter([5], 0, 10)).toEqual([5]);
     })
 
-    test('single sound within range gets returned', () => {
-      expect(band_pass_filter([5], 0, 10)).toEqual([5]);
+    test('66 within range gets returned', () => {
+      expect(band_pass_filter([66], 55, 75)).toEqual([66]);
     })
 
-    // test('single sound below range gets set at min', () => {
-    //   expect(band_pass_filter([25], 40, 45)).toEqual([40]);
-    // })
+    test('single sound below range gets set at min', () => {
+      expect(band_pass_filter([25], 40, 45)).toEqual([40]);
+    })
   })
 })

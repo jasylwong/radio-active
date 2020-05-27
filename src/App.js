8415b1ef270c7854be19cfe8546a122a61c9d600
Jason <jasylwong@gmail.com>
2020-05-27 20:28:15 +0100

    deploy if pull request

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Deploy only
+          Deploy only on PR
         </p>
         <a
           className="App-link"

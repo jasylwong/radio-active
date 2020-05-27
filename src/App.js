43d6e34c1a47242b7e654e0b5b994bd117388d9a
Jason <jasylwong@gmail.com>
2020-05-27 23:33:45 +0100

    remove unnecessary React tags

--- src/App.js
+++ src/App.js
@@ -4,19 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Deploy only on PR
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Deploy only on PR
     </div>
   );
 }

9a8df10e0f9af73f7ed52e74ac4024062040e507
Jason <jasylwong@gmail.com>
2020-05-29 20:46:21 +0100

    add api and forward button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Title from './components/Title';
+import './App.css';
 
 function App() {
-  // console.log(document.getElementById('audio').paused)
   const [isPlaying, setIsPlaying] = useState(false);
+  const [stations, setStations] = useState(null);
+  const [station, setStation] = useState('');
+  const [stationNP, setStationNP] = useState('');
+  const [stationCounter, setStationCounter] = useState(0);
+
+  useEffect(() => {
+    axios.get('https://cors-anywhere.herokuapp.com/https://listenapi.planetradio.co.uk/api9/initdadi/absolute-radio-10s')
+      .then((res) => {
+        setStations(res.data.stationBrandRelated);
+        setStation(res.data.stationBrandRelated[0]);
+        setStationNP(res.data.stationBrandRelated[0].stationNowPlaying);
+      });
+  }, []);
 
   const handleClick = () => {
     const audio = document.getElementById('audio');
@@ -12,11 +25,31 @@ function App() {
     return isPlaying ? audio.pause() : audio.play();
   };
 
+  const handleForward = () => {
+    if (stationCounter === stations.length - 1) {
+      setStationCounter(0);
+      setStation(stations[0]);
+    } else {
+      setStationCounter(stationCounter + 1);
+      setStation(stations[stationCounter + 1]);
+    }
+    setStationNP(stations[0].stationNowPlaying);
+  };
+
   return (
     <div className="App">
       <Title content="Radio-Active" />
-      <audio id="audio" src="https://ais.absoluteradio.co.uk/absoluteclassicrock.mp3?direct=true" paused="true" />
+      <br />
+      <div>{`Station: ${station.stationName}`}</div>
+      <br />
+      <img src={stationNP.nowPlayingImage} alt={station.Name} />
+      <audio id="audio" src={station.stationMP3Stream} paused="true" />
+      <br />
+      <br />
+      <p>{`Current track: ${stationNP.nowPlayingTrack} by ${stationNP.nowPlayingArtist}`}</p>
+      <br />
       <button id="player" onClick={() => handleClick()} type="button">{isPlaying ? 'Pause' : 'Play'}</button>
+      <button onClick={() => handleForward()} type="button">Forward</button>
     </div>
   );
 }
